195fd12efcc68a5e32cedf53f0c8e6453425d766
jhon varon <jhonvaronyate@hotmail.com>
2024-05-16 09:11:40 -0500

    The layout of the store was created along with its components home, product.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductComponent, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private productService: ProductsService) {}
+
+  products: Product[] = [];
+
+  ngOnInit() {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', { page: 1, perpage: 5 })
+      .subscribe((products: Products) => {
+        this.products = products.items;
+      });
+  }
+}
